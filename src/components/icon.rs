0c8a29998a144f6fe1b8a10df54f076e38b012c1
martial-plains <41553768+martial-plains@users.noreply.github.com>
2024-09-12 09:52:56 -0400

    Improve responsive design for better mobile and tablet support
    - Adjusted layout and styles for various screen sizes
    - Optimized images and media queries for faster loading times
    - Fixed alignment issues on smaller screens

--- src/components/icon.rs
+++ src/components/icon.rs
@@ -3,9 +3,12 @@ use dioxus::prelude::*;
 use crate::types::Quiz;
 
 #[component]
-pub fn Icon(quiz: ReadOnlySignal<Quiz>) -> Element {
+pub fn Icon(
+    quiz: ReadOnlySignal<Quiz>,
+    #[props(default = String::new(), into)] class: String,
+) -> Element {
     rsx! {
-        div { class: "p-1 rounded-md md:rounded-xl md:w-14 md:h-14 md:p-2 xl:rounded-lg",
+        div { class: "p-1 rounded-md md:rounded-xl md:w-14 md:h-14 md:p-2 xl:rounded-lg {class}",
             img { src: quiz().icon, alt: quiz().icon }
         }
     }
